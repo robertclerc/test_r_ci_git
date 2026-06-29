673405ce7f7b47b7f89ab5bfd99245094e7b8b3d
robertclerc <robertclerc@outlook.fr>
2026-06-29 15:07:24 +0200

    test

--- functions.R
+++ functions.R
@@ -19,20 +19,16 @@ donnees <- c(1, 2, 3, 4, 5)
 res <- calculStat(donnees, 1)
 
 for (i in 1:length(donnees)) {
-  
-  
-  print(donnees[i])}
+  print(donnees[i])
+}
 
 x <- NULL
 
 print(x)
-
+print(x)
 if (is.null(x) == TRUE) {
   print("x est NULL")
 }
-
-
-
 
 
 df <- data.frame(a = c(1, 2, 3), b = c(4, 5, 6))
