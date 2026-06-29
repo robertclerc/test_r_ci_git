365acaabda71df59017b5deacfa5c0848aa526ba
robertclerc <robertclerc@outlook.fr>
2026-06-29 14:49:12 +0200

    feat: ajout test

--- functions.R
+++ functions.R
@@ -1,37 +1,38 @@
 library(dplyr)
 
-calculStat <- function(x,y){
-  
-  result=x+y
-  
-  if(length(result)> 10){
+calculStat <- function(x, y) {
+  result <- x + y
+
+  if (length(result) > 10) {
     print("grand vecteur")
   }
-  
-  if(T){
-    result = result*2
+
+  if (T) {
+    result <- result * 2
   }
-  
-  data.frame(valeur=result)
+
+  data.frame(valeur = result)
 }
 
-donnees <- c(1,2,3,4,5)
+donnees <- c(1, 2, 3, 4, 5)
 
-res=calculStat(donnees,1)
+res <- calculStat(donnees, 1)
 
-for(i in 1:length(donnees)){
+for (i in 1:length(donnees)) {
   print(donnees[i])
 }
 
-x = NULL
+x <- NULL
 
-if(is.null(x)==TRUE){
+print(x)
+
+if (is.null(x) == TRUE) {
   print("x est NULL")
 }
 
-df=data.frame(a=c(1,2,3),b=c(4,5,6))
+df <- data.frame(a = c(1, 2, 3), b = c(4, 5, 6))
 
-df2=df %>% mutate(c=a+b)
+df2 <- df %>% mutate(c = a + b)
 
 print(df2)
 
