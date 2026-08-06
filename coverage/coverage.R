996e6824389c4f0d7fb5669d14643e33e0bb03c0
robertclerc <robertclerc@outlook.fr>
2026-08-06 14:41:25 +0200

    feat: ajout de covr

---
+++ coverage/coverage.R
@@ -0,0 +1,10 @@
+library(covr)
+
+cov <- file_coverage(
+  source_files = c(
+    here::here("R", "calculstats.R")
+  ),
+  test_files = c(
+    here::here("tests", "testthat", "test_lambda.R")
+  )
+)
